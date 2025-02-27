bea1618f293e5b1c3c9a5a5d68db3e3f495c08e2
anush <anush@Anusha.mshome.net>
2025-02-27 19:15:03 +0530

    First Commit

---
+++ jenkinsProject/src/test/java/demo/jenkinsTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class jenkinsTest {
+	@Test
+	public void demo() {
+		Reporter.log("add class added",true);
+		
+	} 
+
+}
